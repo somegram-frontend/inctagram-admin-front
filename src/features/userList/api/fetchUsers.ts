cd2a540600ef0176a5bc1ebbd1dc083fca374bea
Nikolay Kevlich <izmail.k2@gmail.com>
2025-04-15 21:34:45 +0300

    refactor: remove deleteUser

--- src/features/userList/api/fetchUsers.ts
+++ src/features/userList/api/fetchUsers.ts
@@ -1,7 +1,6 @@
 import { client } from "@/shared/api/instanse";
 import { GetUsersQueryVariables } from "@/shared/configs/gql/graphql";
 import { graphql } from "@/shared/configs/gql";
-import { gql } from "graphql-request";
 
 const getUsersQuery = graphql(`
   query getUsers(
@@ -32,30 +31,7 @@ const getUsersQuery = graphql(`
   }
 `);
 
-const deleteUserMutation = gql`
-  mutation deleteUser($userId: String!) {
-    deleteUser(userId: $userId)
-  }
-`;
-
-export type DeleteUserResponse = {
-  deleteUser: string;
-};
-
 export const fetchUsers = async (variables: GetUsersQueryVariables) => {
   const data = await client.request(getUsersQuery, variables);
   return data.getUsers;
-};
-
-export const deleteUser = async (userId: string) => {
-  const variables = { userId };
-  try {
-    const data: DeleteUserResponse = await client.request(
-      deleteUserMutation,
-      variables,
-    );
-    return data.deleteUser;
-  } catch (error) {
-    return Promise.reject(error);
-  }
 };
